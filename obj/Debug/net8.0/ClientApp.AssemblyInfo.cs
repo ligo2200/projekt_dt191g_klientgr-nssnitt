9f1b177ce4a7efd547971e834cc8b9aaada2eb52
Linda Götenmark <ligo2200@student.miun.se>
2024-08-17 18:39:32 +0200

    removed buy-button

--- obj/Debug/net8.0/ClientApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClientApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7deba6c1edac407eeead1c5e479fa6d650f5dde3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb8afe13ebfd33c65d64be06c5900bb9620c66f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
